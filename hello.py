e61a8c4471004eba589aa220b04d3becec449c77
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-07 05:17:51 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,28 @@
+import urllib.request as req
+import json
+
 import streamlit as st
-st.title("hello")
+
+
+# URLや保存ファイル名を指定
+url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json'
+filename = 'tenki.json'
+# ダウンロード
+req.urlretrieve(url, filename)
+
+# ダウンロードしたファイルを開く --- (*1)
+with open('tenki.json', 'r', encoding="UTF-8") as f:
+  data = json.load(f)
+# 読み出したデータを解析 --- (*2)
+for area in data:
+  name = area['name']
+  print("[", name, "]")
+　a=("[", name, "]")
+  for ts in area['srf']['timeSeries']:
+    times = [n for n in ts['timeDefines']]
+    if 'weathers' in ts['areas']:
+      for i,v in enumerate(ts['areas']['weathers']):
+        print(times[i], ":", v)
+        b=(times[i], ":", v)
+
+st.title(a)
