d98ee8e14a58d148617a58a02f43ca8e1aa5266e
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-07 07:31:39 +0900

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -12,6 +12,7 @@ start=dt.date(2021,1,1)
 end=dt.date(2021,3,1)
 code="SP500"
 df=web.DataReader(code,"fred",start,end)
+st.text(df.columns)
 st.write(df.head(100))
 st.write(
     px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
