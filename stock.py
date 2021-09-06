089d75dcd9080178d46d907be36bb09859ef31c6
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-07 08:01:37 +0900

    Create stock.py

---
+++ stock.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import datetime as dt
+import numpy as np
+import pandas_datareader.data as web
+
+import plotly.express as px
+
+st.title('sp500と日経平均株価の比較')
+
+start=dt.date(2011,1,1)
+end=dt.date(2021,9,1)
+code="SP500"
+code2="N225"
+df=web.DataReader(code,"fred",start,end)
+df2=web.DataReader(code2,"fred",start,end)
+st.text(df.columns)
+#st.write(df)
+st.write(
+px.line(df, title="sample figure")
+)
