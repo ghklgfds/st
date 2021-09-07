3181d90eb4ec12c3c50b6fbda6133b5e68c36ab5
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-08 00:21:24 +0900

    Update laborrate.py

--- laborrate.py
+++ laborrate.py
@@ -4,6 +4,6 @@ a=pd.read_csv('TimeSeriesResult_20210907231948299.csv')
 st.title('有効求人倍率統計')
 st.write(a)
 st.text(a.columns)
-for (x,y,z) in zip (a['時点'],a['地域'],a['（季節調整値）有効求人倍率【倍】'])
+for (x,y,z) in zip (a['時点'],a['地域'],a['（季節調整値）有効求人倍率【倍】']):
     st.text(x,y,z)
    
