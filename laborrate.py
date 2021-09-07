6aeb70e5981bc0f27d4795d341169d0445c48d81
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-08 00:15:50 +0900

    Update laborrate.py

--- laborrate.py
+++ laborrate.py
@@ -4,8 +4,8 @@ a=pd.read_csv('TimeSeriesResult_20210907231948299.csv')
 st.title('有効求人倍率統計')
 st.write(a)
 st.text(a.columns)
-for i in a['時点']:
-   c=i.find('2021年7')
+for i in  a:
+   c=i['時点'].find('2021年7')
    if c>-1:
       st.text(i)
       
