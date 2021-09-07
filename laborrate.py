9ee2f7bb5619170c7a08e4e986b2af4dac87123a
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-08 00:31:33 +0900

    Update laborrate.py

--- laborrate.py
+++ laborrate.py
@@ -2,8 +2,9 @@ import pandas as pd
 import streamlit as st
 a=pd.read_csv('TimeSeriesResult_20210907231948299.csv')
 st.title('有効求人倍率統計')
+st.text('全履歴')
 st.write(a)
-st.text(a.columns)
+#st.text(a.columns)
 tank=[]
 for (x,y,z) in zip (a['時点'],a['地域'],a['（季節調整値）有効求人倍率【倍】']):
     c=x.find('2021年7月')
@@ -11,4 +12,5 @@ for (x,y,z) in zip (a['時点'],a['地域'],a['（季節調整値）有効求人
             d=[x,y,z]
             tank.append(d)
 df=pd.DataFrame(tank)
+st.text('2021年7月時点')
 st.write(df)
