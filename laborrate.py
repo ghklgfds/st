24ee6e18c8cbd9a65f0b475cb7a5a9829a6c8e50
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-08 00:26:03 +0900

    Update laborrate.py

--- laborrate.py
+++ laborrate.py
@@ -7,5 +7,7 @@ st.text(a.columns)
 for (x,y,z) in zip (a['時点'],a['地域'],a['（季節調整値）有効求人倍率【倍】']):
     c=x.find('2021年7月')
     if c>-1:
-            st.text(x+y+z)
+            st.text(x)
+            st.text(y)
+            st.text(z)
    
