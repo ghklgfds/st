6f169712c97cd842d0ff17576d017709e82eb187
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-07 23:48:47 +0900

    Update laborrate.py

--- laborrate.py
+++ laborrate.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import streamlit as st
 a=pd.read_csv('TimeSeriesResult_20210907231948299.csv')
+st.title('有効求人倍率統計')
 st.write(a)
-st.text(a.columns)
-for i in a:
-  if i['時点']==['2021年7月']:
-    st.text(i)
-    
+
