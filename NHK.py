3db84793110720d1ce25b9193e7df10998a61f50
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 05:47:15 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -10,7 +10,7 @@ now = datetime.datetime.now()
 #import streamlit as st
 #mon = st.slider('何月?', 1, 12, 9)
 st.title('NHKの出演者から番組を探す')
-day = st.slider('何日?', now.day, 31, now.day)
+day = st.slider('何日?', now.day+1, 31, now.day+1)
 url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-09-'+str(day)+'.json?key=')
 key='lMA29WCIfOF57Gvt5cGi84Ee4RTsI97r'
 a=requests.get(url+key)
