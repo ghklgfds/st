297da5fd1e29d77645c4ca788dbf8cf7d3546338
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-12 22:09:06 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -1,1 +1,37 @@
 
+
+import requests
+import pandas as pd
+import bs4
+import json
+import streamlit as st
+url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-09-13.json?key=')
+key='lMA29WCIfOF57Gvt5cGi84Ee4RTsI97r'
+a=requests.get(url+key)
+
+b = a.json()
+c=(b['list']['g1'])
+tank=[]
+for i in c:
+    for m in i:
+        print(m)
+    start=i['start_time']
+    end=i['end_time']
+    area=i['area']
+    service=i['service']
+    title=(i['title'])
+    subtitle=(i['subtitle'])
+    content=(i['content'])
+    act=i['act']
+    genres=i['genres']
+    data=[start,end,area,service,title,subtitle,content,act,genres]
+    tank.append(data)
+df=pd.DataFrame(tank)
+#df.to_csv('nhk.csv')
+    
+#このJSONオブジェクト)は、連想配列（Dict）っぽい感じのようなので
+#JSONでの名前を指定することで情報がとってこれる
+#print(b)
+
+
+
