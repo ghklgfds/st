95bbd7867529dfbeba2d02e7944346a80e12a431
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 11:19:54 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -40,7 +40,7 @@ df=pd.DataFrame(tank)
                           
 acts=df[6]    
 acttank
-for actdata in acts
+for actdata in acts:
     if actdata==actdata:
         acttank.append(actdata)
         
