8736fdd03c36fa7433684b339a2c93b5eafe045c
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 12:46:51 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -16,11 +16,11 @@ else:
 #mon = st.slider('何月?', 1, 12, 9)
 st.title('NHKの番組を探す')
 if now.day+1>31:
-    maxday=now.day+7-31
+    maxday=now.day+20-31
 else :
     maxday=now.day+7
 #nowday=now.day
-day = st.slider('何日?', now.day, maxday+20,now.day)
+day = st.slider('何日?', now.day, maxday,now.day)
 url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-'+month+'-'+str(day)+'.json?key=')
 key='lMA29WCIfOF57Gvt5cGi84Ee4RTsI97r'
 a=requests.get(url+key)
