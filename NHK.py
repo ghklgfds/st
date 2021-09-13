bec6c8f9698b7a9992f4bd2c093124dda71db899
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 16:36:19 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -12,7 +12,7 @@ import datetime
 
 
 def form_callback():
-    st.write(st.session_state.my_slider)
+    
     st.write(st.session_state.my_checkbox)
 
 
@@ -83,7 +83,7 @@ elif len(mes)>53:
 
 
  with st.form(key='my_form'):
-    slider_input = st.slider('My slider', 0, 10, 5, key='my_slider')
+    
     checkbox_input = st.checkbox('Yes or No', key='my_checkbox')
     submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
 
