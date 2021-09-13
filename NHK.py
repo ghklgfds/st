5332856ee39f04a25186ab28f7f4faa7ce98106d
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 12:57:56 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -26,13 +26,13 @@ key='lMA29WCIfOF57Gvt5cGi84Ee4RTsI97r'
 a=requests.get(url+key)
 if a=='<Response [400]>':
    st.text(a)
-   break
-b = a.json()
+else:  
+ b = a.json()
 #st.text(b)
-c=(b['list']['g1'])
+ c=(b['list']['g1'])
 
-tank=[]
-for i in c:
+ tank=[]
+ for i in c:
     #for m in i:
         #print(m)
     start=i['start_time']
@@ -49,18 +49,18 @@ for i in c:
     data=[starttime,endtime,title,subtitle,content,act]
     tank.append(data)
 #print(tank)
-df=pd.DataFrame(tank)
-titles=df[3]
-acts=df[5]    
-acttank=[]
-for actdata in acts:
+ df=pd.DataFrame(tank)
+ titles=df[3]
+ acts=df[5]    
+ acttank=[]
+ for actdata in acts:
     if actdata!='':
         acttank.append(actdata)
 #selectmethod=st.sidebar.selectbox('method',('タイトル検索','出演者検索'))
 
 #if selectmethod==['タイトル検索']:
-selecttitle=st.sidebar.radio('タイトルで探します。空欄を選ぶと出演者で探せます',(titles))
-for t in tank:
+ selecttitle=st.sidebar.radio('タイトルで探します。空欄を選ぶと出演者で探せます',(titles))
+ for t in tank:
     cnt=1
     
     n=selecttitle
@@ -95,8 +95,8 @@ for t in tank:
               cnt=0
  
 #elif selectmethod==['出演者検索']:
-selectact= st.sidebar.radio("チェックした出演者の番組情報を入手できます",(acttank))
-for v in tank:
+ selectact= st.sidebar.radio("チェックした出演者の番組情報を入手できます",(acttank))
+ for v in tank:
       cnt=1
       if selectact!='':
        p=selectact
