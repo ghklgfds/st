03f5f9fc7dc882e5ae13925f24c5c63f7257f08a
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 13:59:08 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -7,7 +7,8 @@ import streamlit as st
 import time 
 import datetime
 
-key=pd.read_csv('Key.csv')
+Keydf=pd.read_csv('Key.csv')
+key=keydf[0]
 now = datetime.datetime.now()
 if now.month<10:
     month=str(0)+str(now.month)
