c91c0bf0c3998f1b32d0be219a124819ad229485
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 16:34:22 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -7,6 +7,19 @@ import streamlit as st
 import time 
 import datetime
 
+
+
+
+
+def form_callback():
+    st.write(st.session_state.my_slider)
+    st.write(st.session_state.my_checkbox)
+
+
+
+
+
+
 f = open('Key.txt', 'r', encoding='UTF-8')
 
 key = f.read()
@@ -67,6 +80,13 @@ elif len(mes)>53:
     if actdata!='':
         acttank.append(actdata)
  selectmethod=st.sidebar.radio('method',('タイトル検索','出演者検索'))
+
+
+ with st.form(key='my_form'):
+    slider_input = st.slider('My slider', 0, 10, 5, key='my_slider')
+    checkbox_input = st.checkbox('Yes or No', key='my_checkbox')
+    submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
+
  st.text(selectmethod)
  if selectmethod==['タイトル検索']:
   selecttitle=st.sidebar.radio('タイトルで探します。',(titles))
