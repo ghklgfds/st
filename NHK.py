79ba2cc479dc5e8a49cee4323aa1adcc2a5a337d
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-09-13 17:13:00 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -126,7 +126,7 @@ elif len(mes)>53:
 
 
 if st.session_state.my_checkbox==[True]:
-   selectact= st.sidebar.radio("出演者で探します",(acttank))
+    selectact= st.sidebar.radio("出演者で探します",(acttank))
     for v in tank:
       cnt=1
       if selectact!='':
