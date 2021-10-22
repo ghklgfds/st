e4e09b8cb22f2f866fe35b8b55f853a3b7be85d9
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-10-22 17:55:17 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -124,7 +124,8 @@ day = st.slider('何日の番組をお探しですか？', 1, 31,now.day)
 url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-'+month+'-'+str(day)+'.json?key=')
 a=requests.get(url+key)
 mes=(a.text)
-st.text(a)
+st.text(mes[-2:-1])
+
 if len(mes)<53:
    st.text('番組情報がまだありません。')
 elif len(mes)>53:
