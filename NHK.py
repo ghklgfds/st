fe29667d9ea9f6266a2817f8c6ca6e20cd911118
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-11-01 07:57:34 +0900

    Update NHK.py

--- NHK.py
+++ NHK.py
@@ -132,7 +132,7 @@ else:
     #st.text(day)
  
 day = st.slider('何日の番組をお探しですか？', 1, 31,nowday)
-url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-'+str(month)+'-'+str(day)+'.json?key=')
+#url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-'+str(month)+'-'+str(day)+'.json?key=')
 #url=('https://api.nhk.or.jp/v2/pg/list/130/g1/2021-11-01.json?key=')
 a=requests.get(url+key)
 mes=(a.text)
