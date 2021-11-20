b65b2cf470d24f92b4a0d0ff707955fad1878ebb
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2021-11-20 19:10:07 +0900

    Update jimon.py

--- jimon.py
+++ jimon.py
@@ -4,12 +4,9 @@ import streamlit as st
 while True:
     #nayami = input("あなたが尊敬する人がいるとして、その人はなんて言ってますか？")
     nayami=st.text_input('First name')
-    bnayami=input(nayami+"についてどうすべきですか？")
-    print(nayami,bnayami)
-    data=[nayami,bnayami]
-    df=pd.DataFrame(data)
-    df.T.to_csv("nayami.csv",mode="a",index=False,header=False)
-    d=input("続けるならｙを入力してください")
+    bnayami=st.text_input(nayami+"についてどうすべきですか？")
+    st.text(nayami,bnayami)
+    d=st.text_input("続けるならｙを入力してください")
     if d!="ｙ":
         break
     else:
