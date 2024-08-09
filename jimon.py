6f621a6483a50994cc45647dc61cbb88ff6bc43a
ghklgfds <62078160+ghklgfds@users.noreply.github.com>
2024-08-10 07:24:40 +0900

    Update jimon.py

--- jimon.py
+++ jimon.py
@@ -5,7 +5,8 @@ while True:
     #nayami = input("あなたが尊敬する人がいるとして、その人はなんて言ってますか？")
     nayami=st.text_input('First name')
     bnayami=st.text_input(nayami+"についてどうすべきですか？")
-    st.text(nayami,bnayami)
+    st.text(nayami)
+    st.text(bnayami)
     d=st.text_input("続けるならｙを入力してください")
     if d!="ｙ":
         break
